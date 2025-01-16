fe8a5eea17d255a6748dfa65c9462053d1a0e85d
Okada Hayato <peregrin8alde@gmail.com>
2025-01-16 19:21:38 +0900

    tmp

--- doc/sample01/src/sampleapp/src/main/java/sample/application/domain/object/value/CalculationResult.java
+++ doc/sample01/src/sampleapp/src/main/java/sample/application/domain/object/value/CalculationResult.java
@@ -1,13 +1,47 @@
 package sample.application.domain;
 
 public class CalculationResult {
-    private final double value;
+    // 値は不変
+    private final double result;
+    private final double rate;
 
-    public CalculationResult(double value) {
-        this.value = value;
+    // 生成時にバリデーションと値の決定
+    public CalculationResult(double result, double rate) {
+        this.validate();
+        this.result = result;
+        this.rate = rate;
     }
 
-    public double getValue() {
-        return value;
+    // 値の参照
+    public double getResult() {
+        return result;
     }
+
+    public double getRate() {
+        return rate;
+    }
+
+    // 値が一致したら同一扱い
+    @Override
+    public boolean equals(Object obj) {
+        // 同一インスタンスなら当然同一
+        if (this == obj) return true;
+        // 比較対象が null や別クラスなら比較できない
+        if (obj == null || getClass() != obj.getClass()) return false;
+        // 各フィールドの比較
+        CalculationResult calculationResult = (CalculationResult) obj;
+        return result == calculationResult.getStatus()
+            && rate == calculationResult.getCalculationResult();
+    }
+    // equals をオーバーライドする場合は hashCode もオーバーライドする必要がある
+    @Override
+    public int hashCode() {
+        return Objects.hash(result, rate);
+    }
+
+    // 不当な理由に応じた例外を発生させる
+    private void validate() {
+
+    }
+
 }
