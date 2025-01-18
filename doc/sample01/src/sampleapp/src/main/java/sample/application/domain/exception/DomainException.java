95651d6268ed4fe06baacb57abd2ee46c7d9b18a
Okada Hayato <peregrin8alde@gmail.com>
2025-01-18 13:47:21 +0900

    tmp

---
+++ doc/sample01/src/sampleapp/src/main/java/sample/application/domain/exception/DomainException.java
@@ -0,0 +1,18 @@
+package sample.application.domain.exception;
+
+public class DomainException extends Exception {
+    public DomainException() {
+    }
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+    public DomainException(Throwable cause) {
+        super(cause);
+    }
+
+    public DomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
