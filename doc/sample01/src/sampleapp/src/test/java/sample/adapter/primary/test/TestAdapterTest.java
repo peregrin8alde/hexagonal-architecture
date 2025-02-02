ac4129f0427aa80ad14a6817ebc07527b6dc202d
Okada Hayato <peregrin8alde@gmail.com>
2025-02-02 12:04:34 +0900

    tmp

--- doc/sample01/src/sampleapp/src/test/java/sample/adapter/primary/test/TestAdapterTest.java
+++ doc/sample01/src/sampleapp/src/test/java/sample/adapter/primary/test/TestAdapterTest.java
@@ -17,7 +17,7 @@ class TestAdapterTest {
     private TestAdapter testAdapter;
 
     @Test
-    void test01() {
+    void testUseCase01MockRepo() {
         RateRepositoryInterface repository = new MockRateRepo();
         ApplicationServiceInterface app = new UseCase01(repository);
         testAdapter = new TestAdapter(app);
@@ -48,10 +48,10 @@ class TestAdapterTest {
     }
 
     @Test
-    void test02() {
+    void testUseCase01FileRepo() {
         RateRepositoryInterface repository = new FileRateRepo(
                 "src/test/java/sample/adapter/primary/test/file-rate.csv");
-                ApplicationServiceInterface app = new UseCase01(repository);
+        ApplicationServiceInterface app = new UseCase01(repository);
         testAdapter = new TestAdapter(app);
 
         try {
@@ -77,6 +77,33 @@ class TestAdapterTest {
             // 異常系の場合はこの例外をテストする必要がある
             e.printStackTrace();
         }
+    }
+
+    void testUseCase02MockRepo() {
+        RateRepositoryInterface repository = new MockRateRepo();
+        ApplicationServiceInterface app = new UseCase02(repository);
+        testAdapter = new TestAdapter(app);
+
+        // 異常時は例外ではなく復帰値でエラーを返すので try / catch 不要
+        // 異常系も result の内容をチェック
+        List<TestCalculationInput> inputList = new ArrayList<>();
+        List<ApplicationResult> expectedList = new ArrayList<>();
+        for (int input = 0; input <= 100; input++) {
+            inputList.add(new TestCalculationInput(input));
+            double rate = 1.01;
+            ApplicationResult result = new ApplicationResult(input * rate, rate);
+            expectedList.add(result);
+        }
+        for (int input = 101; input <= 200; input++) {
+            inputList.add(new TestCalculationInput(input));
+            double rate = 1.5;
+            ApplicationResult result = new ApplicationResult(input * rate, rate);
+            expectedList.add(result);
+        }
+
+        List<ApplicationResult> actualList = testAdapter.run(inputList);
+
+        assertIterableEquals(expectedList, actualList);
     }
 
 }
